717040b57711c3778ceb1a434ba1cd6fa05fe1e9
ochukoerewive <ochukoerewive@gmail.com>
2021-07-08 05:07:56 +0000

    Create the Flask App

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
-from flask import Flask, render_template
-
+from flask import Flask
 if os.path.exists("env.py"):
     import env
 
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    @app.route("/hello")
-    def hello():
-        return render_template("index.html")
+@app.route("/")
+@app.route("/hello")
+def hello():
+    return "Hello world again!!!"
 
 
-        if __name__ == "__main__":
-            app.run(host=os.environ.get("IP"),
-                port=int(os.environ.get("PORT")), debug=True)
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=True)
