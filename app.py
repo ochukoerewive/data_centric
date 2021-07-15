92055764e3ff5676bb5daa439ae298c4a0a27337
ochukoerewive <ochukoerewive@gmail.com>
2021-07-15 18:47:34 +0000

    DATA_CENTRIC

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 
 mongo = PyMongo(app)
 
@@ -19,7 +19,7 @@ mongo = PyMongo(app)
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks = tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
