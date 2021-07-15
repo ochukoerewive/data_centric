add7a30847c5afeed2b38657014551b1103f0492
ochukoerewive <ochukoerewive@gmail.com>
2021-07-15 06:07:02 +0000

    DATA_CENTRIC

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, flash, render_template, url_for, redirect, request, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-@app.route("/hello")
-def hello():
-    return render_template("index.html")
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks = tasks)
+
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
